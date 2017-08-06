7aee19b949035dc29686da300deaa950bb4abb3b
matbur95 <mateusz@burniak.pl>
2017-08-06 11:44:54 +0200

    Improve split

--- inz/tests/test_utils.py
+++ inz/tests/test_utils.py
@@ -6,8 +6,17 @@ from inz import split
 
 def test_split_list_int():
     ints = list(range(7))
-    want = [[0, 1, 2], [3, 4, 5]]
+    want = [[0, 1, 2], [3, 4, 5], [6]]
     get = list(split(ints, 3))
+    assert len(get) == len(want)
+    assert get == want
+
+
+def test_split_list_int_greater_width():
+    ints = list(range(3))
+    want = [[0, 1, 2]]
+    get = list(split(ints, 4))
+    assert len(get) == len(want)
     assert get == want
 
 
@@ -15,24 +24,66 @@ def test_split_list_str():
     strings = list(map(str, range(6)))
     want = [['0', '1'], ['2', '3'], ['4', '5']]
     get = list(split(strings, 2))
+    assert len(get) == len(want)
     assert get == want
 
 
 def test_split_ndarray_int():
     array = np.arange(10, dtype=int).reshape(-1, 2)
     want = [np.array([[0, 1], [2, 3]]),
-            np.array([[4, 5], [6, 7]])]
+            np.array([[4, 5], [6, 7]]),
+            np.array([[8, 9]])]
     get = list(split(array, 2))
     assert len(get) == len(want)
-    assert all([(i == j).all() for i, j in zip(get, want)])
+    assert all([np.array_equal(i, j) for i, j in zip(get, want)])
 
 
 def test_split_generator_str():
     strings = map(str, range(6))
     want = [['0', '1'], ['2', '3'], ['4', '5']]
     get = list(split(strings, 2))
+    assert len(get) == len(want)
     assert get == want
 
+def test_split_list_int_not_allow():
+    ints = list(range(7))
+    want = [[0, 1, 2], [3, 4, 5]]
+    get = list(split(ints, 3, False))
+    assert len(get) == len(want)
+    assert get == want
+
+
+def test_split_list_int_greater_width_not_allow():
+    ints = list(range(3))
+    want = []
+    get = list(split(ints, 4, False))
+    assert len(get) == len(want)
+    assert get == want
+
+
+def test_split_list_str_not_allow():
+    strings = list(map(str, range(6)))
+    want = [['0', '1'], ['2', '3'], ['4', '5']]
+    get = list(split(strings, 2, False))
+    assert len(get) == len(want)
+    assert get == want
+
+
+def test_split_ndarray_int_not_allow():
+    array = np.arange(10, dtype=int).reshape(-1, 2)
+    want = [np.array([[0, 1], [2, 3]]),
+            np.array([[4, 5], [6, 7]])]
+    get = list(split(array, 2, False))
+    assert len(get) == len(want)
+    assert all([np.array_equal(i, j) for i, j in zip(get, want)])
+
+
+def test_split_generator_str_not_allow():
+    strings = map(str, range(6))
+    want = [['0', '1'], ['2', '3'], ['4', '5']]
+    get = list(split(strings, 2, False))
+    assert len(get) == len(want)
+    assert get == want
 
 if __name__ == '__main__':
     pytest.main()
--- inz/utils.py
+++ inz/utils.py
@@ -1,17 +1,36 @@
+""" Module contains common functions used in project. """
+
 from typing import Iterable
 
 import numpy as np
 
 
-def split(iterable: Iterable, width):
+def split(iterable: Iterable, width: int, allow_missing=True):
     """ Generator yields iterable in parts.
 
     :param iterable: iterable to split
     :param width: length of each part
+    :param allow_missing: if True last part may be smaller
     """
     it = iter(iterable)
-    while True:
-        rv = [next(it) for _ in range(width)]
+    flag = True
+    while flag:
+        retval = []
+        flag = False
+        for _ in range(width):
+            try:
+                retval.append(next(it))
+            except StopIteration:
+                if not allow_missing:
+                    return
+                break
+        else:
+            flag = True
+
+        if not retval:
+            return
+
         if isinstance(iterable, np.ndarray):
-            rv = np.array(rv)
-        yield rv
+            retval = np.array(retval)
+
+        yield retval

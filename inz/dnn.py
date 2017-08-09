791f0d34f2adafa24f621b2c9ba0b2c6adc59a3b
matbur95 <mateusz@burniak.pl>
2017-08-09 09:35:49 +0200

    Add Layers in DNN

--- inz/dnn.py
+++ inz/dnn.py
@@ -1,6 +1,17 @@
+import numpy as np
+
+from inz import activation as act
+from inz.layers import Layer, fully_connected, input_data
+
+
 class DNN:
-    def __init__(self, network):
+    def __init__(self, network: Layer):
         self.network = network
+
+        layer = network
+        while layer.previous is not None:
+            layer = layer.previous
+        self.input = layer
 
     def fit(self, X_inputs: list, Y_targets: list, n_epoch=10, batch_size=64, shuffle=False):
         pass
@@ -15,7 +26,7 @@ class DNN:
         pass
 
     def predict(self, X: list):
-        pass
+        return self.input.feedforward(X)
 
     def predict_label(self, X: list):
         pass
@@ -25,8 +36,16 @@ class DNN:
 
 
 if __name__ == '__main__':
-    x = []
-    y = []
-    net = None
-    model = DNN(net)
-    model.fit(x, y)
+    x = np.array(([1, 1], [1, 0], [0, 1], [0, 0]), dtype=float)
+    y = np.array(([1, 0], [0, 1], [0, 1], [1, 0]), dtype=float)
+
+    inp = input_data((None, 2))
+    fc1 = fully_connected(inp, 3)
+    fc2 = fully_connected(fc1, 2, activation=act.softmax)
+
+    model = DNN(fc2)
+
+    np_round = np.round(model.predict(x), 3)
+
+    for i, j, k in zip(x, np_round, y):
+        print(i, j, k)

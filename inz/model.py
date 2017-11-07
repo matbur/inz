fe3eafef023af31cec250125cfc8b9b8db1fccff
matbur95 <mateusz@burniak.pl>
2017-11-07 10:25:07 +0100

    Better printing

--- inz/model.py
+++ inz/model.py
@@ -1,4 +1,5 @@
 import json
+from itertools import repeat
 from pathlib import Path
 from time import time
 from typing import Tuple
@@ -29,16 +30,19 @@ class Model:
 
         self.errors = []
 
-    def fit(self, X_inputs: np.ndarray, Y_targets: np.ndarray, validation_set: Tuple[np.ndarray, np.ndarray] = None,
-            n_epoch=10, batch_size=64, shuffle=False):
+    def fit(self, X_inputs: np.ndarray, Y_targets: np.ndarray,
+            validation_set: Tuple[np.ndarray, np.ndarray] = None,
+            learning_rate=None, n_epoch=10, batch_size=64,
+            shuffle=False, train_file='train.json'):
 
         xlen = len(X_inputs)
-        step = 1
+        step = 0
+        order = np.random.permutation(xlen) if shuffle else np.arange(xlen)
+        training = []
+        testing = []
         for epoch in range(1, n_epoch + 1):
-            # p = np.random.permutation(xlen)
-            p = np.arange(xlen)
-            batches_x = split(X_inputs[p], batch_size)
-            batches_y = split(Y_targets[p], batch_size)
+            batches_x = split(X_inputs[order], batch_size)
+            batches_y = split(Y_targets[order], batch_size)
             err = []
             num, den = 0, 0
             for i, (batch_x, batch_y) in enumerate(zip(batches_x, batches_y)):
@@ -57,10 +61,21 @@ class Model:
                 l = len(batch_x)
                 num += l - np.count_nonzero(predict.argmax(axis=1) - batch_y.argmax(axis=1))
                 den += l
+                acc = num / den
+                iteration = i * batch_size
 
                 t = time() - t0
                 print(f'Training Step: {step:<4} | total loss: {e:.5f} | time: {t:.3f}s')
-                print(f'         epoch: {epoch:0>3} | acc: {num/den:.4f} -- iter: {i * batch_size:0>3}/{xlen}')
+                print(f'         epoch: {epoch:0>3} | acc: {acc:.4f} -- iter: {iteration:0>3}/{xlen}')
+                training.append({
+                    'step': step,
+                    'total_loss': e,
+                    'time': t,
+                    'epoch': epoch,
+                    'accuracy': acc,
+                    'iteration': iteration,
+                    'len': xlen,
+                })
 
             test_predict = self.predict(validation_set[0])
             test_y = validation_set[1]
@@ -70,8 +85,32 @@ class Model:
             print('--')
             print(f'End of epoch: {epoch:0>3}   | val_loss: {test_loss:.5f} | val_acc: {test_acc:.4f}')
             print('--')
+            testing.append({
+                'test_loss': test_loss,
+                'epoch': epoch,
+                'test_accuracy': test_acc,
+            })
             mean = np.mean(err)
             self.errors.append(mean)
+        data = json.dumps({'training': training, 'testing': testing})
+        Path(train_file).write_text(data)
+
+    @staticmethod
+    def _parse_learning_rate(lr, n_epoch=None):
+        if isinstance(lr, float):
+            return repeat(lr)
+        if isinstance(lr, (list, tuple)):
+            assert n_epoch
+
+            lr1, lr2 = lr
+
+            def f():
+                x = 0
+                while 1:
+                    yield x * (lr2 - lr1) / (n_epoch - 1) + lr1
+                    x += 1
+
+            return f()
 
     def get_weights(self) -> NetworkSchema:
         data = []
@@ -93,7 +132,7 @@ class Model:
         with open(model_file, 'w') as f:
             json.dump(data, f)
 
-    def predict(self, x: np.ndarray):
+    def predict(self, x: np.ndarray) -> np.ndarray:
         return self.input.feedforward(x)
 
     def predict_label(self, x: np.ndarray):
